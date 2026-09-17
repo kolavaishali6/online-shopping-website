c7731707f667673d1edb23042a39f12ff1022a02
vaishali kola <24811a05b1@gmail.com>
2026-09-17 19:03:49 +0530

    Update backend URL for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        https://online-shopping-backend-qe1n.onrender.com,
         changeOrigin: true,
       },
     },
